da80238487a90b025df28dd2f1f3b1062ccf5e57
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-01 16:25:20 +0700

    docs: add inline info about presets upstream

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -2,8 +2,8 @@
 
 module.exports.plugins = [
 
+  // https://github.com/remarkjs/remark-lint/tree/main/packages/remark-preset-lint-recommended
   // the following rules are set in remark-preset-lint-recommended
-  // require('remark-lint'),
   // require('remark-lint-final-newline'),
   // require('remark-lint-list-item-bullet-indent'),
   // [require('remark-lint-list-item-indent'), 'tab-size'],
@@ -19,6 +19,21 @@ module.exports.plugins = [
   // require('remark-lint-no-shortcut-reference-link'),
   // require('remark-lint-no-undefined-references'),
   // require('remark-lint-no-unused-definitions')
+
+  // https://github.com/remarkjs/remark-lint/tree/main/packages/remark-preset-lint-consistent
+  // the following rules are set in remark-preset-lint-consistent
+  // [require('remark-lint-blockquote-indentation'), 'consistent'],
+  // [require('remark-lint-checkbox-character-style'), 'consistent'],
+  // [require('remark-lint-code-block-style'), 'consistent'],
+  // [require('remark-lint-emphasis-marker'), 'consistent'],
+  // [require('remark-lint-fenced-code-marker'), 'consistent'],
+  // [require('remark-lint-heading-style'), 'consistent'],
+  // [require('remark-lint-link-title-style'), 'consistent'],
+  // require('remark-lint-list-item-content-indent'),
+  // [require('remark-lint-ordered-list-marker-style'), 'consistent'],
+  // [require('remark-lint-rule-style'), 'consistent'],
+  // [require('remark-lint-strong-marker'), 'consistent'],
+  // [require('remark-lint-table-cell-padding'), 'consistent']
 
   require('remark-preset-lint-recommended'),
   require('remark-preset-lint-consistent'),
