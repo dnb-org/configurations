741365a004a92012527299de6e0d8501b90f3caa
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-01 15:29:49 +0700

    chore: initial commit, first release

---
+++ packages/remark-config/README.md
@@ -0,0 +1,5 @@
+This is a remark preset used across DNB projects.
+
+It is based on [remark-preset-lint-recommended](https://github.com/remarkjs/remark-lint/tree/main/packages/remark-preset-lint-recommended) with some additions on it's own.
+
+You probably won't need this ;)
---
+++ packages/remark-config/index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports.plugins = [
+  // require('remark-lint'),
+  // require('remark-lint-final-newline'),
+  // require('remark-lint-list-item-bullet-indent'),
+  // [require('remark-lint-list-item-indent'), 'tab-size'],
+  // require('remark-lint-no-auto-link-without-protocol'),
+  // require('remark-lint-no-blockquote-without-marker'),
+  // require('remark-lint-no-literal-urls'),
+  // [require('remark-lint-ordered-list-marker-style'), '.'],
+  // require('remark-lint-hard-break-spaces'),
+  // require('remark-lint-no-duplicate-definitions'),
+  // require('remark-lint-no-heading-content-indent'),
+  // require('remark-lint-no-inline-padding'),
+  // require('remark-lint-no-shortcut-reference-image'),
+  // require('remark-lint-no-shortcut-reference-link'),
+  // require('remark-lint-no-undefined-references'),
+  // require('remark-lint-no-unused-definitions')
+
+      // ["remark-lint-final-newline", true],
+      // ["remark-lint-linebreak-style", "unix"],
+      // ["remark-lint-first-heading-level", 2],
+      // ["remark-lint-no-html", true],
+      // ["remark-lint-maximum-line-length", 1000],
+      // ["remark-lint-no-file-name-irregular-characters", "\\.a-zA-Z0-9-_"],
+      // ["remark-lint-write-good", ["warn", {
+      //   "passive": false,
+      //   "whitelist": [ "read-only" ]
+      // }]]
+  require('remark-frontmatter'),
+  require('remark-lint-fenced-code-flag')
+
+]
