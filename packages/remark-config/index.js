0ba6ec79bbcb06c92322e4ba1e528c44944fabe4
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-02 22:27:48 +0700

    chore: move to styleguide preset and fix package setup

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -2,22 +2,6 @@
 
 module.exports.plugins = [
 
-  // https://developer.aliyun.com/mirror/npm/package/remark-preset-lint-styleguide
-  // https://github.com/remarkjs/remark-lint/tree/main/packages/remark-preset-lint-consistent
-  // the following rules are set in remark-preset-lint-consistent
-  // [require('remark-lint-blockquote-indentation'), 'consistent'],
-  // [require('remark-lint-checkbox-character-style'), 'consistent'],
-  // [require('remark-lint-code-block-style'), 'consistent'],
-  // [require('remark-lint-emphasis-marker'), 'consistent'],
-  // [require('remark-lint-fenced-code-marker'), 'consistent'],
-  // [require('remark-lint-heading-style'), 'consistent'],
-  // [require('remark-lint-link-title-style'), 'consistent'],
-  // require('remark-lint-list-item-content-indent'),
-  // [require('remark-lint-ordered-list-marker-style'), 'consistent'],
-  // [require('remark-lint-rule-style'), 'consistent'],
-  // [require('remark-lint-strong-marker'), 'consistent'],
-  // [require('remark-lint-table-cell-padding'), 'consistent']
-
   require('remark-preset-lint-styleguide/writability'),
   require('remark-preset-lint-consistent'),
   require('remark-frontmatter'),
