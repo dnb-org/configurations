d85cff11dab6df8797f0d0c345f9b65a273d3aba
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-16 23:59:41 +0700

    chore: remove configuration that is already in remark-preset-lint-styleguide
    #CLOSES #1

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -5,16 +5,9 @@ module.exports.plugins = [
   require('remark-preset-lint-styleguide'),
   require('remark-preset-lint-consistent'),
   require('remark-frontmatter'),
-  require('remark-lint-fenced-code-flag'),
-  [require('remark-lint-final-newline'), true],
   [require('remark-lint-linebreak-style'), 'unix'],
   [require('remark-lint-first-heading-level'), 2],
-  [require('remark-lint-no-html'), true],
   [require('remark-lint-maximum-line-length'), 1000],
-  [
-    require('remark-lint-no-file-name-irregular-characters'),
-    "\\.a-zA-Z0-9-_"
-  ],
   [require('remark-lint-write-good'), 'warn', {
      "passive": false,
      "whitelist": [ "read-only" ]
