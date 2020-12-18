45ee5c91a7310a584621ed8b59e3b1cd0f131cbd
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-18 22:17:48 +0700

    refactor: fix coding style

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports.plugins = [
 
-  require('remark-preset-lint-styleguide'),
-  require('remark-preset-lint-consistent'),
-  require('remark-frontmatter'),
-  [require('remark-lint-linebreak-style'), 'unix'],
-  [require('remark-lint-first-heading-level'), 2],
-  [require('remark-lint-maximum-line-length'), 1000],
-  [require('remark-lint-write-good'), 'warn', {
-    'passive': false,
-    'whitelist': ['read-only']
+  require("remark-preset-lint-styleguide"),
+  require("remark-preset-lint-consistent"),
+  require("remark-frontmatter"),
+  [require("remark-lint-linebreak-style"), "unix"],
+  [require("remark-lint-first-heading-level"), 2],
+  [require("remark-lint-maximum-line-length"), 1000],
+  [require("remark-lint-write-good"), "warn", {
+    "passive": false,
+    "whitelist": ["read-only"]
   }],
-  [require('remark-lint-no-file-name-irregular-characters'), '\\.a-zA-Z0-9-_'],
+  [require("remark-lint-no-file-name-irregular-characters"), "\\.a-zA-Z0-9-_"],
 
 ];
