8bb5b7a6826e044a7f75b605c1a55e2d6936803a
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-02-28 20:51:08 +0700

    chore: setup and fix eslint configuration

--- packages/textlint-config/index.js
+++ packages/textlint-config/index.js
@@ -1,83 +1,60 @@
-"use strict";
-
 module.exports = {
-
-  "rules": {
+  rules: {
     "no-todo": true,
     "textlint-rule-no-start-duplicated-conjunction": true,
     "max-comma": {
-      "max": 3
+      max: 3,
     },
     "no-exclamation-question-mark": true,
     "no-dead-link": {
-      "checkRelative": true,
-      "baseURI": null,
-      "ignore": [],
-      "preferGET": [],
-      "ignoreRedirects": false,
-      "retry": 3,
-      "userAgent": "textlint-rule-no-dead-link/1.0"
+      checkRelative: true,
+      baseURI: null,
+      ignore: [],
+      preferGET: [],
+      ignoreRedirects: false,
+      retry: 3,
+      userAgent: "textlint-rule-no-dead-link/1.0",
     },
     "en-capitalization": {
-      "allowHeading": true,
-      "allowFigures": true,
-      "allowLists": true,
-      "allowWords": []
+      allowHeading: true,
+      allowFigures: true,
+      allowLists: true,
+      allowWords: [],
     },
     "stop-words": {
-      "defaultWords": true,
-      "skip": [
-        "Blockquote"
-      ],
-      "exclude": [
-        "utilize",
-        "period of time"
-      ]
+      defaultWords: true,
+      skip: ["Blockquote"],
+      exclude: ["utilize", "period of time"],
     },
     "common-misspellings": {
-      "ignore": []
+      ignore: [],
     },
     "date-weekday-mismatch": true,
-    "terminology": {
-      "defaultTerms": true,
-      "skip": [
-        "Blockquote"
-      ],
-      "exclude": [
-        "CSS"
-      ]
+    terminology: {
+      defaultTerms: true,
+      skip: ["Blockquote"],
+      exclude: ["CSS"],
     },
-    "alex": {
-      "allow": [
-        "boogeyman-boogeywoman"
-      ]
+    alex: {
+      allow: ["boogeyman-boogeywoman"],
     },
     "abbr-within-parentheses": true,
-    "spelling": {
-      "language": "en-gb",
-      "skipPatterns": [
-        "JavaScript"
-      ],
-      "wordDefinitionRegexp": "/[\\w\"]+/g",
-      "suggestCorrections": true
+    spelling: {
+      language: "en-gb",
+      skipPatterns: ["JavaScript"],
+      wordDefinitionRegexp: '/[\\w"]+/g',
+      suggestCorrections: true,
     },
     "unexpanded-acronym": {
-      "ignore_acronyms" : [
-        "DNB",
-        "OSS",
-        "NPM"
-      ]
+      ignore_acronyms: ["DNB", "OSS", "NPM"],
     },
     "footnote-order": true,
     "@textlint-rule/no-unmatched-pair": true,
     "@textlint-rule/no-invalid-control-character": true,
     "no-empty-section": true,
     "no-start-duplicated-conjunction": {
-      "interval": 2
+      interval: 2,
     },
   },
-  "plugins": [
-    "html"
-  ]
-
+  plugins: ["html"],
 };
