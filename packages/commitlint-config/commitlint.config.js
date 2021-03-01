61431bae9a47c1c8b7e754eefaeac47a75c27819
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-03-01 22:07:16 +0700

    chore: prepare release and configure package

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,27 +1,2 @@
-module.exports = {
-  "extends": [
-    "@commitlint/config-conventional"
-  ],
-  "rules": {
-    "body-max-line-length": [
-      0,
-      "always",
-      100
-    ],
-    "type-enum": [
-      0,
-      "always",
-      [
-        "content",
-        "docs",
-        "feat",
-        "fix",
-        "layouts",
-        "refactor",
-        "test",
-        "chore",
-        "wip"
-      ]
-    ]
-  }
-};
+const defaultRemarkConfig = require('./index');
+module.exports = defaultRemarkConfig;
