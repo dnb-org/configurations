8f85e0784f52ee2d34879518127a578f6170da1f
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-03-01 21:36:48 +0700

    chore: prepare build system and update dependencies

--- packages/standard-version-config/.versionrc.js
+++ packages/standard-version-config/.versionrc.js
@@ -1,14 +1,2 @@
-module.exports = {
-  "skip": {
-    "changelog": true
-  },
-  "scripts": {
-    "prerelease": "./bin/release-hook-prerelease.sh"
-  },
-  "bumpFiles": [
-    {
-      "filename": "package.json",
-      "type": "json"
-    }
-  ]
-};
+const defaultStandardVersion = require('index.js');
+module.exports = defaultStandardVersion;
