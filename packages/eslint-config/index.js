d2c65b68b26f14c87c19d09b80589c6b4a98bb10
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-03-04 20:59:40 +0700

    fix: add new configuration to exported configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,16 @@
 module.exports = {
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:compat/recommended"
+  ],
+  plugins: [
+    "prettier"
+  ],
   rules: {
     "prettier/prettier": "error",
   },
+  env: {
+    "browser": true
+  }
 };
