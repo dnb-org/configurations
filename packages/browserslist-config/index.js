8810031e8ef5757240f710f3973a0bd9a6ec48a9
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-03-24 21:54:22 +0700

    chore: remove android and legacy edge as well as update safari/ios versions

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -6,13 +6,9 @@ const supportedBrowsers = [
   "not dead",
   "Chrome >= 60",
   "Firefox >= 60",
-  // needed since Legacy Edge still has usage; 79 was the first Chromium Edge version
-  // should be removed in the future when its usage drops or when it is moved to dead browsers
-  "not Edge < 79",
   "Firefox ESR",
-  "iOS >= 10",
-  "Safari >= 10",
-  "Android >= 6",
+  "iOS >= 12",
+  "Safari >= 12",
   "not Explorer <= 11",
 ];
 
