2bb81b89f78203ddcc823a84d4f256db30399678
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-05-23 12:10:47 +0700

    feat: add changelog creation

--- packages/standard-version-config/index.js
+++ packages/standard-version-config/index.js
@@ -1,14 +1,28 @@
 module.exports = {
-  "skip": {
-    "changelog": true
+  // for available options see https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md
+  // skip: {
+  //   changelog: true,
+  // },
+  scripts: {
+    prerelease: "./bin/release-hook-prerelease.sh",
   },
-  "scripts": {
-    "prerelease": "./bin/release-hook-prerelease.sh"
-  },
-  "bumpFiles": [
+  bumpFiles: [
     {
-      "filename": "package.json",
-      "type": "json"
-    }
-  ]
+      filename: "package.json",
+      type: "json",
+    },
+  ],
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "docs", section: "Documentation" },
+    { type: "style", section: "Styling" },
+    { type: "refactor", section: "Refactors" },
+    { type: "perf", section: "Performance" },
+    { type: "test", section: "Tests" },
+    { type: "build", section: "Build System" },
+    { type: "ci", section: "CI" },
+    { type: "chore", section: "Chore" },
+    { type: "revert", section: "Reverts" },
+  ],
 };
