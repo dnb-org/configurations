e5655e709e2c5baf133c43f53ca03056bb70fef7
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-06-11 22:40:19 +0700

    fix(cypress): fix issues with extending base cypress configuration

--- packages/cypress-config/README.md
+++ packages/cypress-config/README.md
@@ -1,5 +1,11 @@
 ## Usage
 
+Add the configuration package to your setup:
+
+```shell
+npm install -D @dnb-hugo/cypress
+```
+
 Create `tests/cypress/plugins.index.js` with the following content:
 
 ```js
@@ -8,3 +14,14 @@ module.exports = (on, config) => {
   return require('@dnb-hugo/cypress-config')(config.configFile)
 }
 ```
+
+Then add in `cypress.json` at least the following two lines:
+
+```json
+{
+  "extends": "@dnb-hugo/cypress-config/base.json",
+  "pluginsFile": "tests/cypress/plugins/index.js"
+}
+```
+
+This configuration setup is based on [Gleb Bahmutov's blog post](https://www.cypress.io/blog/2020/06/18/extending-the-cypress-config-file/)
--- packages/cypress-config/index.js
+++ packages/cypress-config/index.js
@@ -2,20 +2,26 @@ const deepmerge = require("deepmerge");
 const path = require("path");
 
 function loadConfig(filename) {
+  // console.log("reading config file %s", filename);
   const configJson = require(filename);
   if (configJson.extends) {
-    const baseConfigFilename = path.join(
-      path.dirname(filename),
-      configJson.extends
-    );
+    let baseConfigFilename;
+    if (configJson.extends.startsWith(".")) {
+      baseConfigFilename = path.join(
+        path.dirname(filename),
+        configJson.extends
+      );
+    } else {
+      baseConfigFilename = require.resolve(configJson.extends);
+    }
+    // console.log("config file extends %s", baseConfigFilename);
     const baseConfig = loadConfig(baseConfigFilename);
-    console.log("merging %s with %s", baseConfigFilename, filename);
+    // console.log("merging %s with %s", baseConfigFilename, filename);
     return deepmerge(baseConfig, configJson);
   }
+  // console.log("config file %s does not extend", filename);
+  // console.log(configJson);
   return configJson;
 }
 
-module.exports = (on, config) =>
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  loadConfig(config.configFile);
+module.exports = loadConfig;
