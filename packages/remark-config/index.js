f0d72c22e317df42b07d550b1f8d3081981db9da
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-08-02 12:34:39 +0700

    feat: add ignored files to remark configuration

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -1,17 +1,25 @@
 "use strict";
 
-module.exports.plugins = [
+module.exports = {
 
-  require("remark-preset-lint-styleguide"),
-  require("remark-preset-lint-consistent"),
-  require("remark-frontmatter"),
-  [require("remark-lint-linebreak-style"), "unix"],
-  [require("remark-lint-first-heading-level"), 2],
-  [require("remark-lint-maximum-line-length"), 1000],
-  [require("remark-lint-write-good"), "warn", {
-    "passive": false,
-    "whitelist": ["read-only"]
-  }],
-  [require("remark-lint-no-file-name-irregular-characters"), "\\.a-zA-Z0-9-_"],
+  "settings": {
+    "ignoreName": "node_modules/@dnb-org/remark-config/.remarkignore"
+  },
 
-];
+  "plugins": [
+
+    require("remark-preset-lint-styleguide"),
+    require("remark-preset-lint-consistent"),
+    require("remark-frontmatter"),
+    [require("remark-lint-linebreak-style"), "unix"],
+    [require("remark-lint-first-heading-level"), 2],
+    [require("remark-lint-maximum-line-length"), 1000],
+    [require("remark-lint-write-good"), "warn", {
+      "passive": false,
+      "whitelist": ["read-only"]
+    }],
+    [require("remark-lint-no-file-name-irregular-characters"), "\\.a-zA-Z0-9-_"],
+
+  ];
+
+}
