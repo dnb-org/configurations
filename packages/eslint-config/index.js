958678bc8d69dedc278973270948ee213c19011a
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-11-10 21:14:48 +0700

    feat: add eslint configuration in markdown files

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,6 +1,23 @@
 module.exports = {
-  extends: ["airbnb-base", "prettier", "plugin:compat/recommended"],
-  plugins: ["prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:compat/recommended",
+    "plugin:markdown/recommended",
+  ],
+  overrides: [
+    {
+      files: ["**/*.md"],
+      processor: "markdown/markdown",
+    },
+    {
+      // customize the configuration ESLint uses for ```js
+      // fenced code blocks inside .md files.
+      files: ["**/*.md/*.js"],
+      rules: {},
+    },
+  ],
+  plugins: ["prettier", "markdown"],
   rules: {
     "prettier/prettier": "error",
   },
