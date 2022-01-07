bd688a540742e2708b107d6f13eafc4a51eacfe1
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-01-07 16:08:13 +0700

    feat: remove file extension from pre release hook

--- packages/standard-version-config/index.js
+++ packages/standard-version-config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   // for available options see https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md
   scripts: {
-    prerelease: "./bin/release-hook-prerelease.sh",
+    prerelease: "./bin/release-hook-prerelease",
   },
   bumpFiles: [
     {
