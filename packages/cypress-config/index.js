ad6ac22d1e9eae68c91baa5a5c8bd1c50660ab0d
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-01-19 17:28:30 +0700

    feat: cleanup cypress configuration

--- packages/cypress-config/index.js
+++ packages/cypress-config/index.js
@@ -5,15 +5,9 @@ function loadConfig(filename) {
   // console.log("reading config file %s", filename);
   const configJson = require(filename);
   if (configJson.extends) {
-    let baseConfigFilename;
-    if (configJson.extends.startsWith(".")) {
-      baseConfigFilename = path.join(
-        path.dirname(filename),
-        configJson.extends
-      );
-    } else {
-      baseConfigFilename = require.resolve(configJson.extends);
-    }
+    const baseConfigFilename = configJson.extends.startsWith(".")
+      ? path.join(path.dirname(filename), configJson.extends)
+      : require.resolve(configJson.extends);
     // console.log("config file extends %s", baseConfigFilename);
     const baseConfig = loadConfig(baseConfigFilename);
     // console.log("merging %s with %s", baseConfigFilename, filename);
