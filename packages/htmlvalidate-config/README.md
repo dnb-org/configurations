1814d341bf15a87602146436ec5d2fbb334c3a68
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-03-13 20:27:46 +0700

    chore(config): setup htmlvalidate configuration

---
+++ packages/htmlvalidate-config/README.md
@@ -0,0 +1,7 @@
+# `@dnb-org/htmlvalidate-config`
+
+## Rules
+
+This configuration extends the following setup on the [htmlvalidate-recommended](https://html-validate.org/rules/presets.html) configuration:
+
+- to be written.
--- packages/htmlvalidate-config/index.js
+++ packages/htmlvalidate-config/index.js
@@ -1,19 +1,6 @@
 module.exports = {
   extends: ["html-validate:recommended"],
   rules: {
-    "void-style": 0,
-    "no-inline-styles": 0,
-    "svg-focusable": 0,
-    "no-trailing-whitespace": 0,
-    "no-inline-style": 0,
-    "doctype-html": 0,
-    "attr-quotes": 0,
-    "prefer-tbody": 0,
-    "no-dup-id": 0,
-    "long-title": 0,
-    "element-required-attributes": 0,
-    "prefer-native-element": 0,
-    "element-required-content": 0,
-    "wcag/h30": 0,
+
   },
 };
