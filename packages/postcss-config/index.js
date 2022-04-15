44ade2a456dcb9d95f9ef508ad9988842ada569a
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-04-15 20:23:06 +0700

    feat(fix): remove purgecss

--- packages/postcss-config/index.js
+++ packages/postcss-config/index.js
@@ -1,18 +1,10 @@
 const cssnano = require("cssnano");
-const purgecss = require("@fullhuman/postcss-purgecss");
 const stylelint = require("stylelint");
 const postcsspresetenv = require("postcss-preset-env");
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
   plugins: [
-    purgecss({
-      content: ["./hugo_stats.json"],
-      defaultExtractor: (content) => {
-        const els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.tags, els.classes, els.ids);
-      },
-    }),
     stylelint(),
     autoprefixer(),
     postcsspresetenv({
