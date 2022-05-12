7d9d8bdbc40e0b29761bd89b9d4c7ea24f9015a8
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-05-12 23:40:06 +0700

    chore(config): set up htmlvalidate options as errors

--- packages/htmlvalidate-config/index.js
+++ packages/htmlvalidate-config/index.js
@@ -1,6 +1,58 @@
 module.exports = {
   extends: ["html-validate:recommended"],
   rules: {
-
+    "long-title": [
+      "error",
+      {
+        maxlength: 100,
+      },
+    ],
+    "valid-id": [
+      "error",
+      {
+        relaxed: true,
+      },
+    ],
+    "no-inline-style": [
+      "error",
+      {
+        include: ["lite-youtube"],
+      },
+    ],
+    "require-sri": [
+      "error",
+      {
+        target: "crossorigin",
+      },
+    ],
+    "no-unknown-elements": "error",
+    "no-style-tag": "error",
+    "no-missing-references": "error",
+    "missing-doctype": "error",
+    "input-missing-label": "error",
+    "heading-level": [
+      "error",
+      {
+        allowMultipleH1: false,
+        minInitialRank: "h1",
+        sectioningRoots: ["dialog", '[role="dialog"]'],
+      },
+    ],
+    "class-pattern": [
+      "error",
+      {
+        pattern: "kebabcase",
+      },
+    ],
+    "attr-pattern": "error",
+    "allowed-links": [
+      "error",
+      {
+        allowExternal: true,
+        allowRelative: false,
+        allowAbsolute: true,
+        allowBase: false,
+      },
+    ],
   },
 };
