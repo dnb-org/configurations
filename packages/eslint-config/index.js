96d63eda4addb3525d745ce763158c6dda66cabd
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-05-24 21:16:14 +0700

    chore(deps): update dependencies

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -9,21 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:sonarjs/recommended',
-  ],
-  overrides: [
-    {
-      files: ['**/*.md'],
-      processor: 'markdown/markdown',
-    },
-    {
-      // customize the configuration ESLint uses for ```js
-      // fenced code blocks inside .md files.
-      files: ['**/*.md/*.js'],
-      rules: {
-        'no-console': 'off',
-        'import/no-unresolved': 'off',
-      },
-    },
+    "plugin:package-json/recommended",
   ],
   plugins: [
     'anti-trojan-source',
@@ -31,18 +17,9 @@ module.exports = {
     'no-loops',
     'sonarjs',
     'html',
+    'package-json',
   ],
   ignorePatterns: ['**/node_modules/*', '**/vendor/*'],
-  rules: {
-    'anti-trojan-source/no-bidi': 'error',
-    'no-loops/no-loops': 'error',
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/ban-ts-comment': 0,
-    'import/no-unresolved': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-  },
   settings: {
     'html/html-extensions': ['.html', '.htm'],
     'html/xml-extensions': ['.xml'],
@@ -53,4 +30,28 @@ module.exports = {
     browser: true,
     es2022: true,
   },
+  rules: {
+    'anti-trojan-source/no-bidi': 'error',
+    'no-loops/no-loops': 'error',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-ts-comment': 0,
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.md'],
+      processor: 'markdown/markdown',
+    },
+    {
+      // configuration for ```js fenced code blocks inside .md files.
+      files: ['**/*.md/*.js'],
+      rules: {
+        'no-console': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
 };
