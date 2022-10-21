93364a72b026dd7f76e73e87de1ca4bffe4d9df7
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-10-21 22:16:19 +0700

    feat: adding commitlint configuration

---
+++ packages/commitlint-config/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "extends": [
+    "@commitlint/config-conventional"
+  ],
+  "rules": {
+    "body-max-line-length": [
+      0,
+      "always",
+      100
+    ],
+    "type-enum": [
+      0,
+      "always",
+      [
+        "content",
+        "docs",
+        "feat",
+        "fix",
+        "layouts",
+        "refactor",
+        "test",
+        "chore",
+        "wip"
+      ]
+    ]
+  }
+};
