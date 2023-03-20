57b6fd2dd62f70e10708872b301c1e06c2e31ce0
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-03-20 19:03:24 +0700

    stylelint: update packages and resort rules

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -199,19 +199,6 @@ const config = {
         ]
       }
     ],
-    "scss/at-extend-no-missing-placeholder": null,
-    "scss/at-function-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
-    "scss/at-import-no-partial-leading-underscore": true,
-    "scss/at-import-partial-extension-blacklist": [
-      "scss"
-    ],
-    "scss/at-mixin-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
-    "scss/at-rule-no-unknown": true,
-    "scss/dollar-variable-colon-space-after": "always",
-    "scss/dollar-variable-colon-space-before": "never",
-    "scss/dollar-variable-pattern": "^[_]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
-    "scss/percent-placeholder-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
-    "scss/selector-no-redundant-nesting-selector": true,
     "selector-attribute-quotes": "always",
     "selector-class-pattern": [
       "^[a-z0-9\\-]+$",
@@ -262,7 +249,21 @@ const config = {
           "inline-box"
         ]
       }
-    ]
+    ],
+
+    "scss/at-extend-no-missing-placeholder": null,
+    "scss/at-function-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/at-import-no-partial-leading-underscore": true,
+    "scss/at-import-partial-extension-blacklist": [
+      "scss"
+    ],
+    "scss/at-mixin-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/at-rule-no-unknown": true,
+    "scss/dollar-variable-colon-space-after": "always",
+    "scss/dollar-variable-colon-space-before": "never",
+    "scss/dollar-variable-pattern": "^[_]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/percent-placeholder-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/selector-no-redundant-nesting-selector": true,
   },
   "order/order": [
     "custom-properties",
