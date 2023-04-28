c63e0b6d6e1831d5c223f85fcead9ef3ea658dd2
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-04-28 23:19:11 +0700

    chore(browserslist): update browsers configuration

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,15 +1,16 @@
 // https://github.com/browserslist/browserslist#readme
 // https://browsersl.ist/
 module.exports = [
-  ">= 0.3%",
+  ">= 0.5%",
+  "last 2 major versions",
   "not dead",
   "supports es6-module",
-  "last 2 major versions",
   "Chrome >= 60",
   "Firefox >= 60",
   "Firefox ESR",
   "iOS >= 12",
   "Safari >= 12",
+  "not Explorer <= 11",
   "not op_mini all",
   "not op_mob > 0",
   "not opera > 0",
