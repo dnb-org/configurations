c927a31e6c30770c90df04ba418665bfd7faa3c9
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-04-10 18:42:53 +0700

    feat: rework eslint configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,4 +1,10 @@
+// @ts-check
+
 import fs from 'fs';
+import eslintjs from '@eslint/js';
+import globals from 'globals';
+import markdown from 'eslint-plugin-markdown';
+
 const path = './tsconfig.eslint.json';
 let projectValue = '';
 if (fs.existsSync(path)) {
@@ -6,51 +12,31 @@ if (fs.existsSync(path)) {
 }
 
 export default [
+  eslintjs.configs.recommended,
+  markdown.configs.recommended,
   {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      project: projectValue,
-      extraFileExtensions: ['json'],
+    name: 'dnb/default',
+    // files: '',
+    // ignores: '',
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        //   var1: 'writable',
+        //   var2: 'readonly'
+      },
+      parserOptions: {
+        impliedStrict: true,
+        project: projectValue, // keep in for now, this belongs to typescript setup
+        extraFileExtensions: ['json'],
+      },
     },
-    plugins: [
-      'anti-trojan-source',
-      'html',
-      'no-loops',
-      'sonarjs',
-      // https://github.com/sindresorhus/eslint-plugin-unicorn
-      "unicorn",
-      "jsdoc"
-    ],
-    ignorePatterns: [
-      '**/node_modules/*',
-      '**/vendor/*',
-      '**/.git/*'
-    ],
-    settings: {
-      'html/html-extensions': ['.html', '.htm'],
-      'html/xml-extensions': ['.xml'],
-      'html/indent': '0',
-      'html/report-bad-indent': 'error',
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
     },
-    // https://eslint.org/docs/user-guide/configuring/language-options
-    env: {
-      browser: true,
-      es2022: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:compat/recommended',
-      'plugin:markdown/recommended',
-      'plugin:import/recommended',
-      'plugin:import/typescript',
-      'plugin:sonarjs/recommended',
-      'plugin:toml/standard',
-      "plugin:jsdoc/recommended-error"
-    ],
     rules: {
-      "indent": ["error", "space"],
+      'indent': ['error', 'space'],
       'anti-trojan-source/no-bidi': 'error',
       'no-loops/no-loops': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
@@ -58,71 +44,11 @@ export default [
       '@typescript-eslint/ban-ts-comment': 0,
       '@typescript-eslint/no-var-requires': 'off',
       'import/no-unresolved': 'off',
-
-      //////////////////////////////////////////////
-      // "jsdoc/check-access": 1, // Recommended
-      // "jsdoc/check-alignment": 1, // Recommended
-      // "jsdoc/check-examples": 1,
-      // "jsdoc/check-indentation": 1,
-      // "jsdoc/check-line-alignment": 1,
-      // "jsdoc/check-param-names": 1, // Recommended
-      // "jsdoc/check-property-names": 1, // Recommended
-      // "jsdoc/check-syntax": 1,
-      // "jsdoc/check-tag-names": 1, // Recommended
-      // "jsdoc/check-types": 1, // Recommended
-      // "jsdoc/check-values": 1, // Recommended
-      // "jsdoc/empty-tags": 1, // Recommended
-      // "jsdoc/implements-on-classes": 1, // Recommended
-      // "jsdoc/informative-docs": 1,
-      // "jsdoc/match-description": 1,
-      // "jsdoc/multiline-blocks": 1, // Recommended
-      // "jsdoc/no-bad-blocks": 1,
-      // "jsdoc/no-blank-block-descriptions": 1,
-      // "jsdoc/no-defaults": 1,
-      // "jsdoc/no-missing-syntax": 1,
-      // "jsdoc/no-multi-asterisks": 1, // Recommended
-      // "jsdoc/no-restricted-syntax": 1,
-      // "jsdoc/no-types": 1,
-      // "jsdoc/no-undefined-types": 1, // Recommended
-      // "jsdoc/require-asterisk-prefix": 1,
-      // "jsdoc/require-description": 1,
-      // "jsdoc/require-description-complete-sentence": 1,
-      // "jsdoc/require-example": 1,
-      // "jsdoc/require-file-overview": 1,
-      // "jsdoc/require-hyphen-before-param-description": 1,
-      // "jsdoc/require-jsdoc": 1, // Recommended
-      // "jsdoc/require-param": 1, // Recommended
-      // "jsdoc/require-param-description": 1, // Recommended
-      // "jsdoc/require-param-name": 1, // Recommended
-      // "jsdoc/require-param-type": 1, // Recommended
-      // "jsdoc/require-property": 1, // Recommended
-      // "jsdoc/require-property-description": 1, // Recommended
-      // "jsdoc/require-property-name": 1, // Recommended
-      // "jsdoc/require-property-type": 1, // Recommended
-      // "jsdoc/require-returns": 1, // Recommended
-      // "jsdoc/require-returns-check": 1, // Recommended
-      // "jsdoc/require-returns-description": 1, // Recommended
-      // "jsdoc/require-returns-type": 1, // Recommended
-      // "jsdoc/require-throws": 1,
-      // "jsdoc/require-yields": 1, // Recommended
-      // "jsdoc/require-yields-check": 1, // Recommended
-      // "jsdoc/sort-tags": 1,
-      // "jsdoc/tag-lines": 1, // Recommended
-      // "jsdoc/valid-types": 1 // Recommended
     },
-    overrides: [
-      {
-        files: ['**/*.md'],
-        processor: 'markdown/markdown',
-      },
-      {
-        // configuration for ```js fenced code blocks inside .md files.
-        files: ['**/*.md/*.js'],
-        rules: {
-          'no-console': 'off',
-          'import/no-unresolved': 'off',
-        },
-      },
+    ignorePatterns: [
+      '**/node_modules/*',
+      '**/vendor/*',
+      '**/.git/*',
     ],
   }
 ];
