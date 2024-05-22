e886f1099eabe4c8103de4c95211f67467170d13
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-05-22 17:41:31 +0700

    config: update local remark configuration
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/remark-config/.remarkrc.js
+++ packages/remark-config/.remarkrc.js
@@ -1,15 +1,19 @@
+/**
+ * @typedef {import('unified').Preset} Preset
+ */
+
 /** @type {Preset} */
 import defaultRemarkConfig from '@davidsneighbour/remark-config';
 
 /** @type {Preset} */
-const localRemarkConfig = [
-    // add your local changes here
-];
+// const localRemarkConfig = [
+//     // add your local changes here
+// ];
 
 /** @type {Preset} */
 const mergedConfiguration = {
-    ...defaultRemarkConfig,
-    ...localRemarkConfig,
+  ...defaultRemarkConfig,
+  //  ...localRemarkConfig,
 };
 
 export default mergedConfiguration;
