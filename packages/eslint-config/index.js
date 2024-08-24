6b5846c9ce880c9371c00e6e863c03a607336d3c
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-08-25 06:49:12 +0700

    fix: update eslint markdown plugin to latest version
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,13 +1,13 @@
 // @ts-check
 import eslint from '@eslint/js';
-import markdown from "eslint-plugin-markdown";
+import markdown from "@eslint/markdown";
 import defaultConfig from './configs/default.js';
 import markdownConfig from './configs/markdown.js';
 
 export default [
-  eslint.configs.all,
   // @ts-ignore
   ...markdown.configs.recommended,
+  eslint.configs.all,
   defaultConfig,
   markdownConfig,
 ];
