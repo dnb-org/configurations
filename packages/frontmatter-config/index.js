3a5607f810c24a19346b1d0a82c75ab3b7ead6e4
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-09-23 19:21:30 +0700

    config(frontmatter): add common config options
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/frontmatter-config/index.js
+++ packages/frontmatter-config/index.js
@@ -2,17 +2,25 @@ import path from 'node:path';
 import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({
-  path: path.join(process.cwd(), ".env")
+  path: path.join(process.cwd(), '.env'),
 });
 
 export default async (/** @type {any} */ config) => {
   return {
     ...config,
-    "frontMatter.extends": [
-      "https://dnbhub.xyz/frontmatter/settings.global.json"
+    'frontMatter.extends': [
+      'https://dnbhub.xyz/frontmatter/settings.global.json',
     ],
-    "frontMatter.preview.host": `https://${process.env.HOSTNAME}:${process.env.PORT}`,
-    "frontMatter.taxonomy.seoTitleLength": process.env.SEO_TITLE_LENGTH,
-    "frontMatter.site.baseURL": `${process.env.WEBSITE_URL}`,
+    'frontMatter.preview.host': `https://${process.env.HOSTNAME}:${process.env.PORT}`,
+    'frontMatter.taxonomy.seoTitleLength': process.env.SEO_TITLE_LENGTH,
+    'frontMatter.site.baseURL': `${process.env.WEBSITE_URL}`,
+    'frontMatter.framework.id': 'hugo',
+    'frontMatter.content.publicFolder': 'static',
+    'frontmatter.content.autoUpdateDate': true,
+    'frontMatter.dashboard.content.card.fields.date': true,
+    'frontMatter.taxonomy.dateFormat': "YYYY-MM-dd'T'HH:mm:ssxxx",
+    'frontMatter.taxonomy.noPropertyValueQuotes': ['date'],
+    'frontMatter.taxonomy.dateField': 'publishDate',
+    'frontMatter.taxonomy.modifiedField': 'lastmod',
   };
 };
