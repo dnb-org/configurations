58395eeb2d998b933f8f8cfd6ef68faa54ce0d75
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-10-05 01:00:03 +0700

    feat(postcss): move to esm format
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/postcss-config/index.js
+++ packages/postcss-config/index.js
@@ -1,16 +1,61 @@
-const cssnano = require("cssnano");
-const postcsspresetenv = require("postcss-preset-env");
-const autoprefixer = require("autoprefixer");
+import postcsspresetenv from 'postcss-preset-env';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 
-module.exports = {
+// running purgecss only in production so we can use all available classes in development
+const purgecss =
+  process.env.HUGO_ENVIRONMENT === 'production'
+    ? // @ts-ignore
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./hugo_stats.json'],
+        // https://github.com/gohugoio/hugo/issues/10338
+        // https://discourse.gohugo.io/t/purgecss-and-highlighting/41021
+        safelist: {
+          greedy: [/highlight/, /chroma/, /widget--web-vitals/, /dark/],
+        },
+        fontFace: true,
+        //variables: true,
+        keyframes: true,
+        defaultExtractor: (/** @type {string} */ content) => {
+          const els = JSON.parse(content).htmlElements;
+          return [
+            ...(els.tags || []),
+            ...(els.classes || []),
+            ...(els.ids || []),
+          ];
+        },
+      })
+    : null;
+
+export default {
   plugins: [
+    // https://github.com/anandthakker/doiuse
+    // doiuse({
+    //   browsers: [
+    //     "extends @davidsneighbour/browserslist-config",
+    //   ],
+    //   ignore: ['rem'],
+    //   ignoreFiles: ['**/normalize.css'],
+    // }),
+    purgecss,
+    // https://github.com/postcss/autoprefixer
     autoprefixer(),
+    // https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env
+    // @ts-ignore
     postcsspresetenv({
-      stage: 2
+      stage: 2,
+      browsers: ['extends @davidsneighbour/browserslist-config'],
+      // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
+      features: {
+        'nesting-rules': true,
+      },
+      //debug: true,
     }),
+
+    // https://cssnano.github.io/cssnano
     cssnano({
       preset: [
-        "default",
+        'default',
         {
           discardComments: {
             removeAll: true,
