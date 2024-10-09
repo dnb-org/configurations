12f509e5e5fc5ef5679d24982070a4b47b020ead
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-10-09 17:01:17 +0700

    fix(eslint-config): remove eslint-plugin-anti-trojan-source
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/eslint-config/configs/default.js
+++ packages/eslint-config/configs/default.js
@@ -1,45 +1,43 @@
+import globals from "globals";
+import stylisticJs from "@stylistic/eslint-plugin";
 
-import globals from 'globals';
-import stylisticJs from '@stylistic/eslint-plugin'
+import fs from "node:fs";
 
-import fs from 'node:fs';
-
-const path = './tsconfig.eslint.json';
-let projectPath = '';
+const path = "./tsconfig.eslint.json";
+let projectPath = "";
 if (fs.existsSync(path)) {
-  projectPath = path;
+	projectPath = path;
 }
 
 export default {
-  name: 'dnb/default',
-  //files: ["**/!(*.*)"],
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      //   var1: 'writable',
-      //   var2: 'readonly'
-    },
-    parserOptions: {
-      impliedStrict: true,
-      project: projectPath, // keep in for now, this belongs to typescript setup
-      extraFileExtensions: ['json'],
-    },
-  },
-  linterOptions: {
-    reportUnusedDisableDirectives: 'error',
-  },
-  // processor: {},
-  plugins: {
-    '@stylistic/js': stylisticJs
-  },
-  rules: {
-    '@stylistic/js/indent': ['error', 2],
-    'anti-trojan-source/no-bidi': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/ban-ts-comment': 0,
-    '@typescript-eslint/no-var-requires': 'off',
-    'import/no-unresolved': 'off',
-  },
-  // settings: {},
-}
+	name: "dnb/default",
+	//files: ["**/!(*.*)"],
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			//   var1: 'writable',
+			//   var2: 'readonly'
+		},
+		parserOptions: {
+			impliedStrict: true,
+			project: projectPath, // keep in for now, this belongs to typescript setup
+			extraFileExtensions: ["json"],
+		},
+	},
+	linterOptions: {
+		reportUnusedDisableDirectives: "error",
+	},
+	// processor: {},
+	plugins: {
+		"@stylistic/js": stylisticJs,
+	},
+	rules: {
+		"@stylistic/js/indent": ["error", 2],
+		"@typescript-eslint/no-unused-vars": "error",
+		"@typescript-eslint/no-explicit-any": "error",
+		"@typescript-eslint/ban-ts-comment": 0,
+		"@typescript-eslint/no-var-requires": "off",
+		"import/no-unresolved": "off",
+	},
+	// settings: {},
+};
