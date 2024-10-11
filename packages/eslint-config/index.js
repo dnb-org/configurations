d824343dd6978049adff561716b73df86c229375
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-10-11 11:02:07 +0700

    eslint(feat): add eslint-plugin-security
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,25 +1,26 @@
 // @ts-check
-import eslint from "@eslint/js";
-import defaultConfig from "./configs/default.js";
-import markdownConfig from "./configs/markdown.js";
-import jsonConfig from "./configs/json.js";
-import stylisticJs from "@stylistic/eslint-plugin";
+
+import defaultConfig from './configs/default.js'
+import eslint from '@eslint/js'
+import jsonConfig from './configs/json.js'
+import markdownConfig from './configs/markdown.js'
+import pluginSecurity from 'eslint-plugin-security'
+import stylisticJs from '@stylistic/eslint-plugin'
 
 export default [
-	eslint.configs.all,
-	stylisticJs.configs.customize({
-		// the following options are the default values
-		indent: 2,
-		quotes: "single",
-		semi: false,
-		jsx: true,
-		// ...
-	}),
-	{
-		name: "dnb/ignores",
-		ignores: ["**/node_modules/*", "**/vendor/*", "**/.git/*"],
-	},
-	defaultConfig,
-	...markdownConfig,
-	...jsonConfig,
-];
+  eslint.configs.all,
+  pluginSecurity.configs.recommended,
+  stylisticJs.configs.customize({
+    indent: 2,
+    jsx: true,
+    quotes: 'single',
+    semi: false,
+  }),
+  {
+    ignores: ['**/node_modules/*', '**/vendor/*', '**/.git/*'],
+    name: 'dnb/ignores',
+  },
+  defaultConfig,
+  ...markdownConfig,
+  ...jsonConfig,
+]
