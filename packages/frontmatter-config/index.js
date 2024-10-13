87bffa8f0307a886a52b52902f2f7527c676daf8
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-10-13 17:11:17 +0700

    chore: rework into commonjs package
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- packages/frontmatter-config/index.js
+++ packages/frontmatter-config/index.js
@@ -1,26 +1,25 @@
-import path from 'node:path';
-import { config as dotenvConfig } from 'dotenv';
+const path = require('node:path');
+const { config: dotenvConfig } = require('dotenv');
 
 dotenvConfig({
   path: path.join(process.cwd(), '.env'),
 });
 
-export default async (/** @type {any} */ config) => {
-  return {
-    ...config,
-    'frontMatter.extends': [
-      'https://dnbhub.xyz/frontmatter/settings.global.json',
-    ],
-    'frontMatter.preview.host': `https://${process.env.HOSTNAME}:${process.env.PORT}`,
-    'frontMatter.taxonomy.seoTitleLength': process.env.SEO_TITLE_LENGTH,
-    'frontMatter.site.baseURL': `${process.env.WEBSITE_URL}`,
-    'frontMatter.framework.id': 'hugo',
-    'frontMatter.content.publicFolder': 'static',
-    'frontmatter.content.autoUpdateDate': true,
-    'frontMatter.dashboard.content.card.fields.date': true,
-    'frontMatter.taxonomy.dateFormat': "YYYY-MM-dd'T'HH:mm:ssxxx",
-    'frontMatter.taxonomy.noPropertyValueQuotes': ['date'],
-    'frontMatter.taxonomy.dateField': 'publishDate',
-    'frontMatter.taxonomy.modifiedField': 'lastmod',
-  };
-};
+// @ts-ignore
+module.exports = async (config) => ({
+  ...config,
+  'frontMatter.extends': [
+    'https://dnbhub.xyz/frontmatter/settings.global.json',
+  ],
+  'frontMatter.preview.host': `https://${process.env.HOSTNAME}:${process.env.PORT}`,
+  'frontMatter.taxonomy.seoTitleLength': process.env.SEO_TITLE_LENGTH,
+  'frontMatter.site.baseURL': `${process.env.WEBSITE_URL}`,
+  'frontMatter.framework.id': 'hugo',
+  'frontMatter.content.publicFolder': 'static',
+  'frontmatter.content.autoUpdateDate': true,
+  'frontMatter.dashboard.content.card.fields.date': true,
+  'frontMatter.taxonomy.dateFormat': "YYYY-MM-dd'T'HH:mm:ssxxx",
+  'frontMatter.taxonomy.noPropertyValueQuotes': ['date'],
+  'frontMatter.taxonomy.dateField': 'publishDate',
+  'frontMatter.taxonomy.modifiedField': 'lastmod',
+});
